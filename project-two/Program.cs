ac875f3340024227abc37080058273b48521674f
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2025-11-11 21:35:02 +0200

    finished prj2

---
+++ project-two/Program.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static HashSet<string> InputSet(string name)
+    {
+        Console.Write($"Введіть елементи множини {name} (через пробіл): ");
+        string input = Console.ReadLine();
+        return new HashSet<string>(input.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    static void PrintSet(string name, HashSet<string> set)
+    {
+        Console.Write($"{name} = {{ ");
+        Console.Write(string.Join(", ", set.OrderBy(x => x)));
+        Console.WriteLine(" }");
+    }
+
+    static HashSet<string> InputAndValidateSet(string name, HashSet<string> universalSet)
+    {
+        HashSet<string> currentSet;
+        List<string> invalidElements;
+
+        do
+        {
+            currentSet = InputSet(name);
+
+            invalidElements = currentSet.Except(universalSet).ToList();
+
+            if (invalidElements.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ПОМИЛКА: Елементи {{ {string.Join(", ", invalidElements)} }} відсутні в універсальній множині U.");
+                Console.WriteLine($"Будь ласка, введіть множину {name} ще раз.");
+                Console.ResetColor();
+            }
+
+        } while (invalidElements.Any());
+
+        return currentSet;
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+
+        HashSet<string> U = InputSet("U ");
+
+        HashSet<string> A = InputAndValidateSet("A", U);
+        HashSet<string> B = InputAndValidateSet("B", U);
+        HashSet<string> C = InputAndValidateSet("C", U);
+
+        // 1. Різниця A і B (A \ B)
+        HashSet<string> differenceAB = new HashSet<string>(A.Except(B));
+
+        // 2. Різниця B і C (B \ C)
+        HashSet<string> differenceBC = new HashSet<string>(B.Except(C));
+
+        // 3. Об'єднання результатів (A \ B) ∪ (B \ C)
+        HashSet<string> result = new HashSet<string>(differenceAB.Union(differenceBC));
+
+        Console.WriteLine("\nРезультати:");
+        PrintSet("U", U);
+        PrintSet("A", A);
+        PrintSet("B", B);
+        PrintSet("C", C);
+        Console.WriteLine("---");
+        PrintSet("(A \\ B)", differenceAB);
+        PrintSet("(B \\ C)", differenceBC);
+        PrintSet("(A \\ B) ∪ (B \\ C)", result);
+    }
+}
---
+++ project-two/README.md
@@ -0,0 +1,11 @@
+# ProjectTwo (C#)
+
+Мінімальний C# консоле-проєкт у цій папці.
+
+Build & run (PowerShell):
+
+```powershell
+cd e:\NUWGP\dis-math\project-two
+dotnet build
+dotnet run
+```
